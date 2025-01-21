a09e31340ad7d87768bd5926682b8507b33945c3
kphudson13 <kphudson@live.ca>
2025-01-21 12:40:22 -0500

    Update Taxonomy

--- CleanAndTree.R
+++ CleanAndTree.R
@@ -9,24 +9,29 @@ rawdata <- read.csv("StressDataRaw.csv")
 # Build Tree --------------------------------------------------------------
 
 StressData <- rawdata %>% 
-  filter(Species != "Cebus apella", 
-         Species != "Gerbillus andersoni allenbyi",
-         Species != "Gerbillus piridium")
+  filter(Species != "Gerbillus piridium") #Cant find this species 
 
-StressData$Species[StressData$Species == "Choeropsis liberiensis"] <- "Hexaprotodon liberiensis"
+#update taxonomy from the data set
 StressData$Species[StressData$Species == "Spermophilus columbianus"] <- "Urocitellus columbianus"
+StressData$Species[StressData$Species == "Papio hamadryas ursinus"] <- "Papio ursinus" #Represents a species complex
+StressData$Species[StressData$Species == "Cebus apella"] <- "Sapajus apella"
+StressData$Species[StressData$Species == "Gerbillus andersoni allenbyi"] <- "Gerbillus andersoni"
 
 taxa <- tnrs_match_names(unique(StressData$Species)) #match names with open tree taxonomy 
-
-unmatched <- taxa[is.na(taxa$ott_id), ]
-if (nrow(unmatched) > 0) {
-  print("Unmatched species:")
-  print(unmatched$search_string)
-}
 
 tree <- tol_induced_subtree(ott_ids = taxa$ott_id) #pull the subtree of the matched names 
 # is_in_tree(ott_id(taxa)) #check that only the taxa that are in the synthetic tree
 tree <- compute.brlen(tree, method = "Grafen", power=1) #compute branch lengths, grafen method
+
+#Clean tree tip labels 
+tree$tip.label <- strip_ott_ids(tree$tip.label, remove_underscores = T)
+
+#Update taxonomy from tree of life
+tree$tip.label[tree$tip.label == "Capra hircus (species in domain Eukaryota)"] <- "Capra aegargrus hircus"
+tree$tip.label[tree$tip.label == "Hexaprotodon liberiensis"] <- "Choeropsis liberiensis"
+
+#to view the lists lining up
+cbind(sort(tree$tip.label), sort(unique(StressData$Species)))
 
 write.nexus(tree, file = "Outputs/StressTree_AllSpecies.nex")
 
