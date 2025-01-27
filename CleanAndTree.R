e42196b13bc506ad1e2fa1af415ef5e3bed114e6
kphudson13 <kphudson@live.ca>
2025-01-27 17:13:32 -0500

    Jan 27 Update

--- CleanAndTree.R
+++ CleanAndTree.R
@@ -17,6 +17,8 @@ StressData$Species[StressData$Species == "Spermophilus columbianus"] <- "Urocite
 StressData$Species[StressData$Species == "Papio hamadryas ursinus"] <- "Papio ursinus" #Represents a species complex
 StressData$Species[StressData$Species == "Cebus apella"] <- "Sapajus apella"
 StressData$Species[StressData$Species == "Gerbillus andersoni allenbyi"] <- "Gerbillus andersoni"
+StressData$Species[StressData$Species == "Capra aegargrus hircus"] <- "Capra hircus"
+
 
 taxa <- tnrs_match_names(unique(StressData$Species)) #match names with open tree taxonomy 
 
@@ -28,7 +30,7 @@ tree <- compute.brlen(tree, method = "Grafen", power=1) #compute branch lengths,
 tree$tip.label <- strip_ott_ids(tree$tip.label, remove_underscores = T)
 
 #Update taxonomy from tree of life
-tree$tip.label[tree$tip.label == "Capra hircus (species in domain Eukaryota)"] <- "Capra aegargrus hircus"
+tree$tip.label[tree$tip.label == "Capra hircus (species in domain Eukaryota)"] <- "Capra hircus"
 tree$tip.label[tree$tip.label == "Hexaprotodon liberiensis"] <- "Choeropsis liberiensis"
 
 #to view the lists lining up
@@ -44,3 +46,20 @@ png("Outputs/FullTree_PGLS.png",
     res = 200)
 plot(tree)
 dev.off()
+
+
+
+
+#NCBI access is only available with an API key stored in the .Rprofile
+rawtaxa <- classification(c("Elephas maximus", "Sapajus apella"), db = "ncbi")
+
+
+table <- tibble(names = names(rawtaxa), rawtaxa)
+
+
+# tibble(names = names(rawtaxa), rawtaxa) %>% 
+#   unnest() %>% 
+#   filter(rank %in% c("phylum","class","order","family","genus")) %>% 
+#   select(-id) %>% 
+#   spread(rank, name) %>% 
+#   select(name = rawtaxa, phylum, class, order, family, genus)
