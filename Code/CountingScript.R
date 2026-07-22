103069ea47019fdd9812458f141362e38ab52b33
kphudson13 <kphudson@live.ca>
2026-07-22 16:11:13 -0600

    Combined analysis considerations

--- Code/CountingScript.R
+++ Code/CountingScript.R
@@ -7,16 +7,19 @@
 ################################################################################
 
 library(tidyverse)
-Crtsn <- read.csv("Corticosterone/CrtstnDataClean.csv")
+rm(list=ls()) #clear environment
 
+Crtsn <- read.csv("Corticosterone/CrtstnDataClean.csv")
 Cort <- read.csv("Cortisol/CortisolDataClean.csv")
+FGC <- read.csv("FGCAnalysis/FGCDataClean.csv")
 
 # FGC <- read.csv("FGCAnalysis/FGCDataClean.csv") %>%
 #   .[ , -c(1, 19)]
 
 
-combined <- rbind(Crtsn, Cort)
+combined <- rbind(Crtsn, Cort) # just row bind cort and crst to count 
 
-length(unique(combined$Species))
+length(unique(combined$Species)) # number of species total
+length(unique(combined$Family)) # number of families total
 
-length(unique(combined$Family))
+length(unique(FGC$Species)) # number of species if we were to combine cort and crst with only one point each species
